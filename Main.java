0af998c02f0aecfd7f3593466bb97630afd80ff9
Riyathakur235 <riyathakur8052@gmail.com>
2025-04-08 10:35:21 +0530

    hello

---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+      //byte a=12;
+      //short b =(byte)a;
+      //System.out.println(a); 
+      //int c=6;
+      //short d=(short)c;
+      //System.out.println(d);
+      //double e=34;
+      //byte f=(byte)e;
+      //System.out.println(f);
+      //double a=34;
+      //int b=(int)a;
+      float a=4.4f;
+      byte b=(byte)a;
+      System.out.println(b);
+    }
+}
---
+++ char.java
@@ -0,0 +1,7 @@
+public class char {
+    public static void main(String[] args) {
+       String str =" Hello World  ";
+       System.out.println(str.charAt(7));
+        System.out.println(str.trim().charAt(7));
+    }
+}
