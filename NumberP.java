8eaacbd589703bd39fd5ccf9368898195bb5191b
Riyathakur235 <riyathakur8052@gmail.com>
2025-04-29 14:09:26 +0530

    prime

--- NumberP.java
+++ NumberP.java
@@ -9,12 +9,41 @@ public class NumberP {
     //public static void main(String[] args) {
       //  Print(6,35);
     //}
-    public static void Reverse(int a) {
-       for(int i=a;i>0;i--){
-        System.out.println(i);
-       }
-    }
-    public static void main(String[] args) {
-        Reverse(12);
-    }
+   // public static void Reverse(int a) {
+     //  for(int i=a;i>0;i--){
+       // System.out.println(i);
+       //}
+    //}
+    //public static void main(String[] args) {
+     //   Reverse(12);
+    //}
+   // public static void Count(int a) {
+     // int count=0;
+      //while(a>0){
+        //int digit=a%10;
+        //if(digit>=count){
+          //a=a/10;
+        //}
+        //count++;
+      //}
+      //System.out.println(count);
+  //}
+    //public static void main(String[] args) {
+      //Count(57);
+    //}
+ //public static void Reverse(int a) {
+//int reverse=0;
+  //while(a>0){
+    //int digit=a%10;
+    //a=a/10;
+    //reverse=reverse*10+digit;
+ // }
+  //System.out.println(reverse);
+ //}
+ //public static void main(String[] args) {
+  //Reverse(68);
+ //}
+public static void Palindrom(int a) {
+  
+}
 }
