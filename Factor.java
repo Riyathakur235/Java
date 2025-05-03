c44b920795b0b22c344f849b23584148ff344164
Riyathakur235 <riyathakur8052@gmail.com>
2025-05-03 22:01:52 +0530

    false

--- Factor.java
+++ Factor.java
@@ -3,8 +3,7 @@ public class Factor {
     public static void find(int n) {
         for(int i=1;i<=n;i++){
             if(n%i==0){
-                System.out.println(i+" ");
-                
+                System.out.println(i+" "); 
             }
         }
     }
