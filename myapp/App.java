47c5833c9723f6995558fc609d9084d1b16c72f3
Riyathakur235 <riyathakur8052@gmail.com>
2025-05-09 09:54:16 +0530

    Lcm

---
+++ myapp/App.java
@@ -0,0 +1,9 @@
+package myapp;
+
+public class App {
+    int area;
+    void Rectangle(int l,int w){
+        area=l*w;
+        System.out.println(area);
+    }
+}
---
+++ myapp/main.java
@@ -0,0 +1,9 @@
+
+package myapp;
+public class main {
+    public static void main(String[] args) {
+       App obj=new App();
+       obj.Rectangle(7,9);
+       
+    }
+}
