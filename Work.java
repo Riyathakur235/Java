9b8f8e413dd4c793f30ffd40d5b668662c324005
Riyathakur235 <riyathakur8052@gmail.com>
2025-05-23 10:46:04 +0530

    type

---
+++ Work.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class Work {
+    public static void main(String[] args) {
+        //Collection Fram work topic
+        ArrayList<String> arr= new ArrayList<>();
+        arr.add("A");
+        arr.add("r");
+        arr.add("m");
+        Collection.sort(arr);
+        System.out.println(arr);
+        // arr.add(0,"jain");
+        // System.out.println(arr.get(2));
+        // System.out.println(arr.remove(1));
+        // System.out.println(arr);
+        // System.out.println(arr.size());
+        // for(String str:arr){
+        //  System.out.println(arr);
+        // }
+    }
+}
