4caf7a3acebf2c9b08e59f8c9157a9c31401fe08
Riyathakur235 <riyathakur8052@gmail.com>
2025-06-27 02:02:43 +0530

    in

---
+++ Quicsort.java
@@ -0,0 +1,47 @@
+public class Quicsort {
+   
+
+
+
+    public static void main(String[] args) {
+        int[] a = {3, 2, 10, 5};
+        heapSort(a);
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + "\t");
+        }
+    }
+
+    public static void heapSort(int[] a) {
+        int n = a.length;
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(a, n, i);
+        }
+        for (int i = n - 1; i > 0; i--) {
+            int temp = a[0];
+            a[0] = a[i];
+            a[i] = temp;
+            heapify(a, i, 0);
+        }
+    }
+
+    public static void heapify(int[] a, int n, int i) {
+        int largest = i;
+        int l = 2 * i + 1;
+        int r = 2 * i + 2;
+        if (l < n && a[l] > a[largest]) {
+            largest = l;
+        }
+        if (r < n && a[r] > a[largest]) {
+            largest = r;
+        }
+        if (largest != i) {
+            int temp = a[i];
+            a[i] = a[largest];
+            a[largest] = temp;
+            heapify(a, n, largest);
+        }
+    }
+}
+
+
+
