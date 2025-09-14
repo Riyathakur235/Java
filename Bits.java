68305c51d04b522f64dcf865a95f66bf0f48a0a0
Riyathakur235 <riyathakur8052@gmail.com>
2025-09-14 13:57:46 +0530

    bit

---
+++ Bits.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Bits {
+  public static void main(String[] args) {
+    //  int n=5;
+    // int pos =2;
+    // int bitMask=1<<pos;
+    // Get  bit operatuon
+    // if((bitMask & n)==0){
+    //     System.out.println("bit was zero");
+    // }else{
+    //    System.out.println("bit was one");
+    // }
+
+    //Set bit operation
+    // i
+    
+    //Clear bit operation
+    // int notBitMask=~(bitMask);
+    // int newwNumber= notBitMask & n;
+    // System.out.println(newwNumber);
+
+    //Update bit operation
+    Scanner sc =new Scanner(System.in);
+    int oper=sc.nextInt();
+    int n=5;
+    int pos=1;
+    int bitMask=1<<pos;
+    if(oper==1){
+        int newwNumber= bitMask|n;
+        System.out.println(newwNumber);
+    }else{
+        int newBitMask=~(bitMask);
+        int newwNumber=newBitMask&n;
+        System.out.println(newwNumber);
+    }
+  }  
+}
